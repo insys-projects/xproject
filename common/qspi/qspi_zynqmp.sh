07ee7fcb8947573542317be9e2907057a8dfe4a5
Karakozov Vladimir <karakozov@gmail.com>
2020-08-10 15:52:43 +0300

    Формируется файл BOOT.BIN и он bcgjkmpetncz для записи в QSPI.

--- common/qspi/qspi_zynqmp.sh
+++ common/qspi/qspi_zynqmp.sh
@@ -5,6 +5,7 @@ source /opt/xilinx/sdk/settings64.sh
 CPU=zynqmp
 BIF=qspi_zynqmp.bif
 
-bootgen -image ${BIF} -arch ${CPU} -w -o i qspi_zynqmp.bin
+bootgen -image ${BIF} -arch ${CPU} -w -o i BOOT.BIN
+cp BOOT.BIN qspi_zynqmp.bin
 
 exit 0
